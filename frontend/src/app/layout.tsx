64e862a93336410ce47af004c25025c05e660e84
de3p <de3pvalorant@gmail.com>
2025-07-20 15:37:43 -0500

    change favicon

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "10k SEALS",
   description: "Revive And Protect Your Driveway",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
